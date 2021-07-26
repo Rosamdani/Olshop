430769ed49354f8e9b407f97c0ee81e2d1d87f30
Rosamdani <86883186+Rosamdani@users.noreply.github.com>
2021-07-26 13:10:46 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,374 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Shop</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/slider.css">
+</head>
+<body>
+    <nav>
+        <div class="atas">
+            <div class="kiri">
+                <ul>
+                    <li>Ikuti Kami </li>
+                    <li><a href="#"><img src="asset/img/fb.png" alt="chart"></a></li>
+                    <li><a href="#"><img src="asset/img/instagram.png" alt="chart"></a></li>
+                    <li><a href="#"><img src="asset/img/youtube.png" alt="chart"></a></li>
+                </ul>
+            </div>
+            <div class="kanan">
+                <ul>
+                    <li><a href="login.php">Login</a></li>
+                    <li><span></span></li>
+                    <li><a href="signup.php">Daftar</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="navigation">
+            <div class="logo">
+                <h3>TOKKO</h3>
+            </div>
+            <div class="cari">
+                <input type="search" name="cari" id="cari" placeholder="Celana jeans pria">
+                <button>Cari</button>
+            </div>
+            <div class="chart">
+                <a href="#"><img src="asset/img/chart.png" alt="chart"></a>
+            </div>
+        </div>
+        <div class="menu">
+            <div class="kiri">
+                <ul>
+                    <li><a href="">Beranda</a></li>
+                    <li><a href="">Kategori</a></li>
+                    <li><a href="">Diskon</a></li>
+                    <li><a href="">Profil</a></li>
+                    <li><a href="">Toko Anda</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Konten Hero -->
+
+    <div class="hero">
+        <div class="atas">
+            <div class="kiri">
+                <div class="main">
+                    <div class="slider">
+                        <ul class="slider-parent">
+                            <li class="images-list" data-slider="1"></li>
+                            <li class="images-list" data-slider="2"></li>
+                            <li class="images-list" data-slider="3"></li>
+                            <li class="images-list" data-slider="4"></li>
+                            <li class="images-list" data-slider="5"></li>
+                            <li class="images-list" data-slider="6"></li>
+                        </ul>
+                        <ol class="buttom-circles">
+                            <li class="buttom-circles-list slider-active" data-slider="1"><i class="fa fa-circle-thin"></i></li>
+                            <li class="buttom-circles-list" data-slider="2"><i class="fa fa-circle-thin"></i></li>
+                            <li class="buttom-circles-list" data-slider="3"><i class="fa fa-circle-thin"></i></li>
+                            <li class="buttom-circles-list" data-slider="4"><i class="fa fa-circle-thin"></i></li>
+                            <li class="buttom-circles-list" data-slider="5"><i class="fa fa-circle-thin"></i></li>
+                            <li class="buttom-circles-list" data-slider="6"></li>
+                        </ol>
+                        <i class="fa fa-chevron-right fa-5x"></i>
+                        <i class="fa fa-chevron-left fa-5x"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="kanan">
+                <img src="asset/img/hero2.png" alt="">
+                <img src="asset/img/hero3.png" alt="">
+            </div>
+        </div>
+        <div class="konten">
+            <div class="box-1">
+                <a href="">
+                    <div class="logo">
+                    <img src="asset/img/mall.png" alt="">
+                    </div>
+                    <p>Super Mall</p>
+                </a>
+            </div>
+            <div class="box-1">
+                <a href="">
+                    <div class="logo">
+                    <img src="asset/img/smartphone.png" alt="">
+                    </div>
+                    <p>Pulsa HP</p>
+                </a>
+            </div>
+            <div class="box-1">
+                <a href="">
+                    <div class="logo">
+                    <img src="asset/img/voucher.png" alt="">
+                    </div>
+                    <p>Super Promo</p>
+                </a>
+            </div>
+            <div class="box-1">
+                <a href="">
+                    <div class="logo">
+                    <img src="asset/img/discount.png" alt="">
+                    </div>
+                    <p>Super Diskon</p>
+                </a>
+            </div>
+            <div class="box-1">
+                <a href="">
+                    <div class="logo">
+                    <img src="asset/img/pln.png" alt="">
+                    </div>
+                    <p>Pulsa Listrik</p>
+                </a>
+            </div>
+            <div class="box-1">
+                <a href="">
+                    <div class="logo">
+                    <img src="asset/img/dress.png" alt="">
+                    </div>
+                    <p>Fashion In</p>
+                </a>
+            </div>
+            <div class="box-1">
+                <a href="">
+                    <div class="logo">
+                    <img src="asset/img/coupon.png" alt="">
+                    </div>
+                    <p>Voucher</p>
+                </a>
+            </div>
+            <div class="box-1">
+                <a href="">
+                    <div class="logo">
+                    <img src="asset/img/cod.png" alt="">
+                    </div>
+                    <p>COD</p>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Gender Sale -->
+
+    <div class="gender">
+        <div class="box-2">
+            <div class="box-3 men">
+                <button>Men Sale</button>
+            </div>
+            <div class="box-3 women">
+                <button>Women Sale</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- Kategori -->
+
+    <div class="kategori">
+        <div class="text">
+            <img src="asset/img/icon/kategori.png" alt="">
+            <h4>Kategori</h4>
+        </div>
+        <div class="box-1">
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/tas.jpg" alt="">
+                </div>
+                <p>Tas Wanita</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/sepatu.jpg" alt="">
+                </div>
+                <p>Sepatu Casual</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/sepeda.jpg" alt="">
+                </div>
+                <p>Sepeda</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/mobil.jpg" alt="">
+                </div>
+                <p>Mobil</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/smartphone.jpg" alt="">
+                </div>
+                <p>Handphone</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/laptop.jpg" alt="">
+                </div>
+                <p>Laptop</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/gaming.jpg" alt="">
+                </div>
+                <p>PC Gaming</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/camera.jpg" alt="">
+                </div>
+                <p>Kamera</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/skincare.jpg" alt="">
+                </div>
+                <p>Kecantikan & Perawatan</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/wanita.jpg" alt="">
+                </div>
+                <p>Baju Wanita</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/aksesoris.jpg" alt="">
+                </div>
+                <p>Aksesoris Wanita</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/men.jpg" alt="">
+                </div>
+                <p>Baju Pria</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/formalmen.jpg" alt="">
+                </div>
+                <p>Baju Formal</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/jaket.jpg" alt="">
+                </div>
+                <p>Jersey & Sweater</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/tas.jpg" alt="">
+                </div>
+                <p>Tas Wanita</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/tas.jpg" alt="">
+                </div>
+                <p>Tas Wanita</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/tas.jpg" alt="">
+                </div>
+                <p>Tas Wanita</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="box-2">
+                <div class="foto">
+                    <img src="asset/img/kategori/tas.jpg" alt="">
+                </div>
+                <p>Tas Wanita</p>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <!-- Produk -->
+
+
+    <!-- Footer -->
+
+    <div class="foot">
+        <div class="box-1">
+            <div class="box-2">
+                <p>Menu</p>
+                <ul>
+                    <li><a href="">Beranda</a></li>
+                    <li><a href="">Kategori</a></li>
+                    <li><a href="">Sponsor</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">Profile</a></li>
+                    <li><a href="">Logout</a></li>
+                </ul>
+            </div>
+            <div class="box-2">
+                <p>Menu</p>
+                <ul>
+                    <li><a href="">Beranda</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Sponsor</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">Profile</a></li>
+                    <li><a href="">Logout</a></li>
+                </ul>
+            </div>
+            <div class="box-2">
+                <p>Sosial Media</p>
+                <ul>
+                    <li><a href="">Facebook</a></li>
+                    <li><a href="">Twitter</a></li>
+                    <li><a href="">Instagram</a></li>
+                    <li><a href="">Line</a></li>
+                    <li><a href="">Youtube</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <small>
+            Copyright &copy; #TanpaNamaTeam - All Right Reserved
+        </small>
+    </footer>
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script  src="js/function.js"></script>
+</html>
---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN | TOKKO</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <div class="container">
+        <div class="box-1">
+            <div class="login">
+                <div class="logo"><h3>TOKKO</h3>
+                </div>
+                <form action="">
+                    <input class="input-control" type="text" name="user" placeholder="Email atau Nomor HP">
+                    <input class="input-control" type="password" name="pass" placeholder="Kata sandi">
+                    <a href="#" align="right">Lupa kata sandi?</a>
+                    <button class="btn-sub" type="submit" name="logSub">Login</button>
+                    <br>
+                    <hr>
+                </form>
+            </div>
+            <div class="signup"></div>
+        </div>
+    </div>
+</body>
+</html>
